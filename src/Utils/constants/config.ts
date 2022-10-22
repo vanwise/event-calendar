9648e24f189904df000cc87b0e9ac241d5247944
Ivan Zhukov <vanwise7@gmail.com>
2022-10-22 15:22:52 +0400

    fix: slash on api url env end

--- src/Utils/constants/config.ts
+++ src/Utils/constants/config.ts
@@ -1,4 +1,9 @@
+function getValidApiUrl() {
+  const url = process.env.REACT_APP_BASE_API_URL?.trim();
+  return url?.at(-1) === '/' ? url.slice(0, -1) : url;
+}
+
 export const config = {
-  BASE_API_URL: process.env.REACT_APP_BASE_API_URL,
+  BASE_API_URL: getValidApiUrl(),
   PUBLIC_VAPID_KEY: process.env.REACT_APP_PUBLIC_VAPID_KEY,
 };
